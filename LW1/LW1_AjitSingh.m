a5698873e4fce87c134684671a0125ef86a12827
20252665 <thisisbhullarsaab@gmail.com>
2026-09-13 01:12:33 +0530

    Create LW1_AjitSingh.m

---
+++ LW1/LW1_AjitSingh.m
@@ -0,0 +1,11 @@
+% Name: Ajit
+% Surname: Singh
+% Group: EDIfu-25/1
+% Date: 11/09/2026
+x = 1:32;
+y = x.^2;
+
+plot(x, y, 'o-r', x, y/3, 'xb')
+title('Dvi funkcijos')
+xlabel('X-ašis')
+ylabel('F_1 [o--] | F_2 [-x-]')
